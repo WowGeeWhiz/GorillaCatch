e21cb808d6475390f34213859de6cb1d16bec891
Ethan Johnson <ethan.d.r.johnson@gmail.com>
2024-08-29 19:00:32 -0400

    Work on getting a simple input setup

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputController : MonoBehaviour
+{
+    Touch touch;
+    Vector2 touchStartPosition, touchEndPosition;
+    bool movePlayer = true;
+    public PlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+        }
+
+        if (touch.phase == TouchPhase.Began)
+        {
+            touchStartPosition = touch.position;
+        }
+        else if (touch.phase == TouchPhase.Moved)
+        {
+            touchEndPosition = touch.position;
+        }
+
+        float x = touchEndPosition.x - touchStartPosition.x;
+        float y = touchEndPosition.y - touchStartPosition.y;
+        if (movePlayer)
+        {
+            player.transform.position += new Vector3(x, 0, 0);
+        }
+
+    }
+}
