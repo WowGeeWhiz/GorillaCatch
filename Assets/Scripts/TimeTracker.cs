fb20997b89f4561734be78fbb130b7506765329b
Ethan Johnson <ethan.d.r.johnson@gmail.com>
2024-11-18 16:58:39 -0500

    Added gorilla models and undid constraint locks on player RB

---
+++ Assets/Scripts/TimeTracker.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimeTracker : MonoBehaviour
+{
+
+    float startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.fixedTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool NewBestTime()
+    {
+        if (!PlayerPrefs.HasKey(SceneManager.GetActiveScene().name + "time") 
+            || (PlayerPrefs.GetFloat(SceneManager.GetActiveScene().name + "time") > Time.fixedTime - startTime))
+            {
+            PlayerPrefs.SetFloat(SceneManager.GetActiveScene().name + "time", Time.fixedTime - startTime);
+            return true;
+        }
+        return false;
+
+    }
+
+
+
+}
